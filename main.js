a00a087da3d29a717f93e14af0db966c74dc961d
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-12-30 08:23:37 +0100

    added js

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
+
 import "./style.css"
+import { getWeather } from "./weather"
+import { ICON_MAP } from "./iconMap"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+    .then(renderWeather)
+    .catch(e => {
+        console.error(e)
+        alert("Error getting weather.")
+    })
+
+function renderWeather({current, daily, hourly}) {
+    renderCurrentWeather(current)
+    renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+// fonction pour aller chercher tous nos data dans le HTML
+// on a commencer par data- dans le fichier html, donc on crée une fonction
+// qui sélectionne les choses qui commencent avec data, puis on pourra changer de manière
+// dynamique ce qui suit (dans la fonction render)
+function setValue(selector, value, {parent = document} = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+    
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, {
+    weekday: "long"
+})
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+function renderDailyWeather(daily) {
+    dailySection.innerHTML = ""
+    daily.forEach(day => {
+        // on copie le template fait dans le html
+        const element = dayCardTemplate.content.cloneNode(true)
+        setValue("temp", day.maxTemp, {parent: element})
+        setValue("date", DAY_FORMATTER.format(day.timestamp), {parent: element})
+        element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+        dailySection.append(element)
+    })
+}
